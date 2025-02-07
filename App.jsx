83d554e24d5b8413fa5e7595e34942402298c5d7
Anshijindal20 <jindalanshika98@gmail.com>
2025-02-07 12:04:48 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Chatbot from "./components/Chatbot.jsx"; 
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Chatbot />
+    </div>
+  );
+}
+export default App;
